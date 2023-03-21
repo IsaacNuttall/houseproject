ecb86681e8d3c7ddfdd61c97281b7172d55c1368
IsaacNuttall <isaacnuttall@gmail.com>
2023-03-21 16:12:58 -0600

    house project

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+
+const {getHouses, createHouse, deleteHouse, updateHouse} = require('./controller')
+
+app.get('/api/houses', getHouses)
+app.post('/api/houses', createHouse)
+app.delete('/api/houses/:id', deleteHouse)
+app.put('/api/houses/:id', updateHouse)
+
+app.listen(4004, () => console.log('Docked at port 4004'))
